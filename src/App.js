15843b557be210599bd9b12bd075e9ff3b480c05
viraj mehta <viraj1messi10@gmail.com>
2022-05-31 16:36:31 +0530

    new properties

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 //import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+//import TextForm from './components/TextForm';
 
 //let name = "Viraj";
 //let company = "Oracle FSS";
@@ -12,7 +13,8 @@ function App() {
       <Navbar title="Viraj" aboutViraj="Achievements"/>
       {/*<Navbar/>*/}
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze"/>
+        {/*<TextForm heading="Enter the text to analyze"/>*/}
+        <About/>
       </div>
     </>
     
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,22 +10,38 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleDownClick = () => {
+        console.log("LowerCase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event) =>{
         console.log("On change");
         setText(event.target.value); //to set a new value dynamically
     }
 
-    const [text, setText]=useState('Enter Text Here');
+    const [text, setText]=useState('');
     //text = "new text"; //Wrong way to change the state
     //setText("Viraj is progressing");
   return (
-    <div>
+    <>  
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to LowerCase</button>
     </div>
+    <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length-1} words and {text.length} characters</p>
+        <p>{0.008* (text.split(" ").length-1)} : Total timetaken to read {text.split(" ").length-1} words</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
 
