776f02fc40cd5f6b0d6b3ca64c4a8784856d3ff5
viraj mehta <viraj1messi10@gmail.com>
2022-06-03 22:23:45 +0530

    alert

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 //import logo from './logo.svg';
 import './App.css';
-import About from './components/About';
+//import About from './components/About';
 import Navbar from './components/Navbar';
-//import TextForm from './components/TextForm';
-
+import TextForm from './components/TextForm';
+import React,{useState} from 'react';
+import Alert from './components/Alert';
 //let name = "Viraj";
 //let company = "Oracle FSS";
 function App() {
+  const [mode,setMode] = useState('dark'); //Whether dark mode is enabled
+  const [mode1,setMode1] = useState('Enable Light Mode')
+  const [mode2,setMode2] = useState('light');
+  const [alert,setAlert] = useState(null);
+
+  const showAlert = (message,type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if(mode==='light'){
+      setMode('dark');
+      setMode1('Enable Light Mode');
+      document.body.style.backgroundColor = 'grey';
+      showAlert("Darkmode has been enabled!","success");
+      setMode2('light')
+    }
+    else{
+      setMode('light');
+      setMode1('Enable Dark Mode');
+      document.body.style.backgroundColor = 'white';
+      showAlert("LightMode has been enabled!","success");
+      setMode2('dark');
+    }
+  }
+  
   return (
     <>
     
-      <Navbar title="Viraj" aboutViraj="Achievements"/>
+      <Navbar title="Viraj" aboutViraj="Achievements" mode={mode} toggleMode={toggleMode} switchText={mode1} mode2={mode2}/>
       {/*<Navbar/>*/}
+      <Alert alert={alert}/>
       <div className="container my-3">
-        {/*<TextForm heading="Enter the text to analyze"/>*/}
-        <About/>
+        <TextForm heading="Enter the text to analyze" mode={mode}/>
+        {/*<About/>*/}
       </div>
     </>
     
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,9 +10,16 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+
     const handleDownClick = () => {
         console.log("LowerCase was clicked" + text);
         let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleClearText = () => {
+        console.log("Clear Text was clicked" + text);
+        let newText = '';
         setText(newText);
     }
 
@@ -26,20 +33,22 @@ export default function TextForm(props) {
     //setText("Viraj is progressing");
   return (
     <>  
-    <div className='container'>
+    <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
         <button className="btn btn-primary mx-2" onClick={handleDownClick}>Convert to LowerCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearText}>Clear Text</button>
+{/*<button className="btn btn-danger mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>*/}
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length-1} words and {text.length} characters</p>
         <p>{0.008* (text.split(" ").length-1)} : Total timetaken to read {text.split(" ").length-1} words</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something to preview"}</p>
     </div>
     </>
   )
